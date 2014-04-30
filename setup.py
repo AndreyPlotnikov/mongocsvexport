356c7d705b62a00967aca041193862c9310f00e2
Andrey Plotnikov <andrey.plotnikov@onetwotrip.com>
2014-04-30 12:27:07 +0400

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mongocsvexport',
+    version='0.1',
+    packages=find_packages(exclude=['tests*']),
+    url='https://github.com/AndreyPlotnikov/mongocsvexport',
+    license='MIT',
+    author='Andrey Plotnikov',
+    author_email='plotnikoff@gmail.com',
+    description='Utility that produces a CSV of data stored in a MongoDB',
+    install_requires=[
+      'pymongo'
+    ],
+    entry_points={
+        "console_scripts": [
+            "mongocsvexport=mongocsvexport:main"
+        ]
+    }
+)

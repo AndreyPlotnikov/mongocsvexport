cf11573dc932e94f2ed3dc83645f8bf567820c05
Andrey Plotnikov <plotnikoff@gmail.com>
2014-05-04 14:16:42 +0400

    List expansion and tests

--- mongocsvexport/__init__.py
+++ mongocsvexport/__init__.py
@@ -1,10 +1,12 @@
-"""Export collection to PostgreSQL compliant csv-file"""
+"""Export collection to csv file"""
 import sys
 import argparse
+import itertools
 import pymongo
 import csv
 
 def get_params(prefix, args):
+    """Extract params with given prefix from args dict"""
     params = {}
     for k,v in args.iteritems():
         if k.startswith(prefix) and v:
@@ -15,69 +17,86 @@ def get_params(prefix, args):
 class MongoExport(object):
 
     defaults = {
-        'output_file': None,  # STDOUT by default
-        'limit': None, # Limit number of document to export
+        'limit': None, # Limit number of documents to export
+        'null_value': '',
     }
 
-    def __init__(self, db_name, coll_name, fields, config):
-        self.db_name = db_name
-        self.coll_name = coll_name
-        self.fields = fields
+    def __init__(self, collection, fields, output, config):
+        self.fields = [f.split('.') for f in fields]
         self.config = self.defaults.copy()
         self.config.update(config)
-        self._mongo = None
-        self._output = None
-        self._writer = None
-
-    def export(self):
-        conn = self._mongo_connect()
-        coll = conn[self.db_name][self.coll_name]
-        limit = self.config['limit']
-        self._init_writer()
-        for i, doc in enumerate(coll.find()):
-            if limit and i >= limit:
-                break
-            row = self._get_row(doc)
-            self._writer.writerow(row)
-
-    def _mongo_connect(self):
-        if self._mongo is not None:
-            return self._mongo
-        mongo_params = get_params('mongo_', self.config)
-        self._mongo = pymongo.MongoClient(**mongo_params)
-        return self._mongo
-
-    def _init_writer(self):
-        if self.config['output_file']:
-            self._output = open(self.config['output_file'], 'w')
-        else:
-            self._output = sys.stdout
+        self.collection = collection
+        self._output = self._init_output(output)
         self._writer = csv.writer(self._output)
 
-    def _get_row(self, doc):
-        row = []
-        for field in self.fields:
+    @classmethod
+    def create(cls, db_name, coll_name, fields, output, config):
+        """Constructor which initialize new mongo connection from config"""
+        mongo_params = get_params('mongo_', config)
+        conn = pymongo.MongoClient(**mongo_params)
+        collection = conn[db_name][coll_name]
+        return cls(collection, fields, output, config)
+
+    def run(self):
+        for doc in self._doc_iter():
+            for row in self._get_rows(doc):
+                self._writer.writerow(row)
+
+    def _init_output(self, output):
+        if isinstance(output, basestring):
+            output = open(output, 'w')
+        return output
+
+    def _doc_iter(self):
+        limit = self.config['limit']
+        for i, doc in enumerate(self.collection.find()):
+            if limit and i >= limit:
+                break
+            yield doc
+
+    def _get_rows(self, doc):
+        # generate output rows for given document
+        rows = (self._get_values(doc, field) for field in self.fields)
+        return itertools.product(*rows)
+
+    def _get_values(self, doc, path):
+        # generate all values for document's field
+        field = path[0]
+        if not field:
+            val = doc
+        else:
             val = doc.get(field)
-            if val is None:
-                val = '\N'
-            elif isinstance(val, unicode):
-                val = val.encode('utf-8')
-            else:
-                val = str(val)
-            row.append(val)
-        return row
+        if not isinstance(val, list):
+            val = [val]
+        if len(path) > 1:
+            for v in val:
+                for subv in self._get_values(v, path[1:]):
+                    yield subv
+        else:
+            for v in val:
+                yield self._serialize(v)
+
+    def _serialize(self, val):
+        if val is None:
+            val = self.config['null_value']
+        elif isinstance(val, unicode):
+            val = val.encode('utf-8')
+        else:
+            val = str(val)
+        return val
+
 
 def main():
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(argument_default=argparse.SUPPRESS)
     parser.add_argument('-d', dest='db_name', required=True,
                         help='Database name')
     parser.add_argument('-c', dest='coll_name', required=True,
                         help='Collection name')
     parser.add_argument('-f', dest='fields', required=True,
                         help='Comma separated list of fields')
-    parser.add_argument('--host', dest='mongo_host', default='localhost',
+    parser.add_argument('--host', dest='mongo_host',
                         help='Mongo connection')
-    parser.add_argument('-o', dest='output_file',
+    parser.add_argument('-o', dest='output_file', default=None,
                         help='Output file name. If not specified print to STDOUT')
     parser.add_argument('--limit', dest='limit', type=int,
                         help='Max number of documents')
@@ -87,8 +106,13 @@ def main():
     fields = [f.strip() for f in fields.split(',')]
     coll_name = args.pop('coll_name')
     db_name = args.pop('db_name')
-    export = MongoExport(db_name, coll_name, fields, args)
-    export.export()
+    output = args.pop('output_file')
+    if output:
+        output = open(output, 'w')
+    else:
+        output = sys.stdout
+    export = MongoExport.create(db_name, coll_name, fields, output, args)
+    export.run()
 
 if __name__ == '__main__':
     main()
---
+++ tests/test_core.py
@@ -0,0 +1,192 @@
+# -*- coding: utf-8 -*-
+import unittest
+from datetime import datetime
+import sys
+from mock import patch, MagicMock
+from StringIO import StringIO
+from mongocsvexport import MongoExport, main
+
+
+class CoreTests(unittest.TestCase):
+
+    def setUp(self):
+        self.output = StringIO()
+
+    def create_instance(self, fields, docs, config={}):
+        class MongoExportPatch(MongoExport):
+            def _doc_iter(self):
+                return iter(docs)
+        export = MongoExportPatch(MagicMock(), fields, self.output, config)
+        return export
+
+    def test_defaults(self):
+        export = self.create_instance(['f1','f2'],
+                                      [{'f1':'foo1', 'f2': 'one,two'},{'f2':'foo2'}], {})
+        export.run()
+        result = self.output.getvalue()
+        self.assertEqual(result,
+                         'foo1,"one,two"\r\n,foo2\r\n')
+
+    def test_unicode(self):
+        export = self.create_instance(['f1','f2'],
+                                      [{'f1':'foo1', 'f2': u'сепулька'}])
+        export.run()
+        self.assertEqual(self.output.getvalue(),
+                         'foo1,\xd1\x81\xd0\xb5\xd0\xbf\xd1\x83\xd0\xbb\xd1\x8c\xd0\xba\xd0\xb0\r\n')
+
+    def test_datetime(self):
+        export = self.create_instance(['f1','f2'],
+                                      [{'f1':datetime(2014,5,1,12,34,10),
+                                        'f2': datetime(2014,5,1,12,0,10,345000)}])
+        export.run()
+        self.assertEqual(self.output.getvalue(),
+                         '2014-05-01 12:34:10,2014-05-01 12:00:10.345000\r\n')
+
+    def test_field_traverse(self):
+        export = self.create_instance(['f1','f2.sub'],
+                                      [{'f1':'foo1', 'f2': {'sub': 'foo2'}}], {})
+        export.run()
+        self.assertEqual(self.output.getvalue(),
+                         'foo1,foo2\r\n')
+
+    def test_trivial_list_expansion(self):
+        export = self.create_instance(['f1','f2.'],
+                                      [{'f1':'foo1', 'f2': ['one','two']}], {})
+        export.run()
+        self.assertEqual(self.output.getvalue(),
+                         'foo1,one\r\nfoo1,two\r\n')
+
+    def test_doc_list_expansion(self):
+        export = self.create_instance(['f1','f2.sub'],
+                                      [{'f1':'foo1', 'f2': [{'sub':'one'},{'sub':'two'}]}], {})
+        export.run()
+        self.assertEqual(self.output.getvalue(),
+                         'foo1,one\r\nfoo1,two\r\n')
+
+    def test_doc_list_expansion_absent_field(self):
+        export = self.create_instance(['f1','f2.sub'],
+                                      [{'f1':'foo1', 'f2': [{'sub':'one'},{'sub2':'two'}]}], {})
+        export.run()
+        self.assertEqual(self.output.getvalue(),
+                         'foo1,one\r\nfoo1,\r\n')
+
+
+    def test_multiple_inner_lists_expansion(self):
+        export = self.create_instance(['f1','f2.sub.sub2'],
+                                      [{'f1':'foo1',
+                                        'f2': [
+                                            {'sub':[{'sub2': 'one'}, {'sub2': 'two'}]},
+                                            {'sub':[{'sub2': 'three'}]}]}], {})
+        export.run()
+        self.assertEqual(self.output.getvalue(),
+                         'foo1,one\r\nfoo1,two\r\nfoo1,three\r\n')
+
+
+class CreateTest(unittest.TestCase):
+    """MongoExport.create constructor test"""
+
+    def test_create(self):
+        output = StringIO()
+        config = {'limit': 100,
+                  'mongo_host': 'localhost:27034'}
+        fields = ['f1']
+        with patch('pymongo.MongoClient') as mongo_mock:
+            export = MongoExport.create('testdb', 'testcoll', fields,
+                                        output, config)
+        self.assertEqual(len(mongo_mock.call_args_list), 1)
+        self.assertEqual(mongo_mock.call_args_list[0][1]['host'], 'localhost:27034')
+        self.assertEqual(export.fields, [['f1']])
+        self.assertEqual(export.config['limit'], 100)
+
+
+class MongoInteropTests(unittest.TestCase):
+
+    def setUp(self):
+        self.collection = MagicMock()
+
+    def create_instance(self, docs, config={}):
+        def find_mock(*args, **kwargs):
+            return docs
+        self.collection.find.side_effect = find_mock
+        export = MongoExport(self.collection, ['f1'], MagicMock(), config)
+        return export
+
+    def test_no_limit(self):
+        export = self.create_instance([{'f1':'foo1'},{'f1':'foo2'}])
+        result = list(export._doc_iter())
+        self.assertEqual(len(result),2)
+        self.assertEqual(result, [{'f1':'foo1'},{'f1':'foo2'}])
+
+    def test_limit(self):
+        export = self.create_instance([{'f1':'foo1'},{'f1':'foo2'}],
+                                      {'limit': 1})
+        result = list(export._doc_iter())
+        self.assertEqual(len(result),1)
+        self.assertEqual(result, [{'f1':'foo1'}])
+
+
+class CmdRunTests(unittest.TestCase):
+
+    @patch('sys.argv', ['mongocsvexport'])
+    def test_empty_run(self):
+        stderr = StringIO()
+        with patch('sys.stderr', stderr):
+            with self.assertRaises(SystemExit):
+                main()
+        output = stderr.getvalue()
+        self.assertTrue('usage:' in output)
+        self.assertTrue('argument -d is required' in output)
+
+    @patch('sys.argv', ['mongocsvexport', '-d', 'testdb'])
+    def test_coll_absent(self):
+        stderr = StringIO()
+        with patch('sys.stderr', stderr):
+            with self.assertRaises(SystemExit):
+                main()
+        output = stderr.getvalue()
+        self.assertTrue('usage:' in output)
+        self.assertTrue('argument -c is required' in output)
+
+    @patch('sys.argv', ['mongocsvexport', '-d', 'testdb', '-c', 'testcoll'])
+    def test_fields_absent(self):
+        stderr = StringIO()
+        with patch('sys.stderr', stderr):
+            with self.assertRaises(SystemExit):
+                main()
+        output = stderr.getvalue()
+        self.assertTrue('usage:' in output)
+        self.assertTrue('argument -f is required' in output)
+
+    def _run_main(self):
+        with patch('mongocsvexport.MongoExport.create') as create_mock:
+            with patch('mongocsvexport.MongoExport.run'):
+                main()
+        return create_mock.call_args[0]
+
+    required_args = ['mongocsvexport', '-d', 'testdb', '-c', 'testcoll', '-f', 'f1.sub,f2']
+
+    @patch('sys.argv',
+           ['mongocsvexport', '-d', 'testdb', '-c', 'testcoll', '-f', 'f1.sub,f2'])
+    def test_required_args(self):
+        args = self._run_main()
+        self.assertEqual(args[0], 'testdb')
+        self.assertEqual(args[1], 'testcoll')
+        self.assertEqual(args[2], ['f1.sub','f2'])
+        self.assertEqual(args[3], sys.stdout)
+        self.assertEqual(args[4], {})
+
+    @patch('sys.argv', required_args + ['--limit', '100'])
+    def test_limit(self):
+        args = self._run_main()
+        self.assertEqual(args[4], {'limit': 100})
+
+    @patch('sys.argv', required_args + ['-o', '/tmp/data.csv'])
+    def test_output_file(self):
+        with patch('__builtin__.open') as open_mock:
+            args = self._run_main()
+        self.assertEqual(open_mock.call_args[0][0], '/tmp/data.csv')
+
+    @patch('sys.argv', required_args + ['--host', 'localhost:27034'])
+    def test_host(self):
+        args = self._run_main()
+        self.assertEqual(args[4], {'mongo_host': 'localhost:27034'})
